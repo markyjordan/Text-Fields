56a57f1e0fa634aa76ee9a9438638942872941bb
Mark Jordan <markybjordan@gmail.com>
2020-04-06 23:20:26 -0700

    Created a new project and added the defoult files to the repo.

---
+++ Text Fields/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Text Fields
+//
+//  Created by Marky Jordan on 4/6/20.
+//  Copyright © 2020 Marky Jordan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
