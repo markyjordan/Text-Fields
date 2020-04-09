48370fe0a2ab0b7cb3aef1634dc47a86de43d488
Mark Jordan <markybjordan@gmail.com>
2020-04-08 23:59:40 -0700

    Updated life cycle.

--- Text Fields/ViewController.swift
+++ Text Fields/ViewController.swift
@@ -10,13 +10,26 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    // MARK: Outlets
+    
     @IBOutlet weak var textField1: UITextField!
     @IBOutlet weak var textField2: UITextField!
     @IBOutlet weak var textField3: UITextField!
     @IBOutlet weak var characterCountLabel: UILabel!
     
+    // MARK: Life Cycle
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        // set the character count label to be hidden
+        characterCountLabel.isHidden = true
+        
+        // set the three delegates
+        self.textField1.delegate = emojiDelegate
+        self.textField2.delegate = colorizerDelegate
+        self.textField3.delegate = self
+        
 
     }
 
