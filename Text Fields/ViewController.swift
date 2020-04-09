48a6346cb53c1cc46a1e0fbbc8aa233ce57a09f5
Mark Jordan <markybjordan@gmail.com>
2020-04-09 00:05:32 -0700

    Implemented UITextFieldDelegate protocol in root view controller class.

--- Text Fields/ViewController.swift
+++ Text Fields/ViewController.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITextFieldDelegate {
 
     // MARK: Outlets
     
@@ -16,6 +16,10 @@ class ViewController: UIViewController {
     @IBOutlet weak var textField2: UITextField!
     @IBOutlet weak var textField3: UITextField!
     @IBOutlet weak var characterCountLabel: UILabel!
+    
+    // MARK: Text Field Delegate objects
+    let emojiDelegate = EmojiTextFieldDelegate()
+    let colorizerDelegate = ColorizerTextFieldDelegate()
     
     // MARK: Life Cycle
     
@@ -29,10 +33,24 @@ class ViewController: UIViewController {
         self.textField1.delegate = emojiDelegate
         self.textField2.delegate = colorizerDelegate
         self.textField3.delegate = self
-        
-
     }
 
+    // MARK: Text Field Delegate Methods
+    
+    func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
 
+        // Figure out what the new text will be, if we return true
+        var newText = textField.text! as NSString
+        newText = newText.replacingCharacters(in: range, with: string) as NSString
+        
+        // hide the label if the newText will be an empty string
+        self.characterCountLabel.isHidden = (newText.length == 0)
+        
+        // Write the length of newText into the label
+        self.characterCountLabel.text = String(newText.length)
+        
+        // returning true gives the text field permission to change its text
+        return true
+    }
 }
 
