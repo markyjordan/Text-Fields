db30381151176fa988751d2d8b8f21ac0e2d829c
Mark Jordan <markybjordan@gmail.com>
2020-04-14 15:00:34 -0700

    Created a new class for a random color delegate.

---
+++ Text Fields/Text Field Delegates/RandomColorDelegate.swift
@@ -0,0 +1,16 @@
+//
+//  RandomColorDelegate.swift
+//  Text Fields
+//
+//  Created by Marky Jordan on 4/14/20.
+//  Copyright © 2020 Marky Jordan. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+// MARK: RandomColorDelegate: NSObject, UITextFieldDelegate
+
+class RandomColorDelegate: NSObject, UITextFieldDelegate {
+    
+}
